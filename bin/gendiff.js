1a6171d90e82f47e4001afe1b0c36fb9a74c79d9
zil130 <index.che@gmail.com>
2022-07-25 21:09:16 -0300

    feat: add option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
