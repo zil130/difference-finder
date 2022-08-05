b4a8f07e8fe6242bf8ff9a0a0f506c7bdb06bee4
zil130 <index.che@gmail.com>
2022-08-05 00:19:41 -0300

    feat: add comparison of flat files (YAML)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import { getObjectFromJsonFile, getSortedKeysWithoutDuplicates, hasKey } from './utils.js';
+import {
+  getSortedKeysWithoutDuplicates, hasKey, getData, getFilenameExtension,
+} from './utils.js';
+import getObjectFromData from './parsers.js';
 
 export default (file1, file2) => {
-  const obj1 = getObjectFromJsonFile(file1);
-  const obj2 = getObjectFromJsonFile(file2);
+  const data1 = getData(file1);
+  const data2 = getData(file2);
+  const filenameExtension1 = getFilenameExtension(file1);
+  const filenameExtension2 = getFilenameExtension(file2);
+  const obj1 = getObjectFromData(data1, filenameExtension1);
+  const obj2 = getObjectFromData(data2, filenameExtension2);
   const keys = getSortedKeysWithoutDuplicates(obj1, obj2);
 
   const getKeyComparison = (acc, key) => {
     let newAcc = '';
+
     if (hasKey(obj1, key) && hasKey(obj2, key)) {
       newAcc = (obj1[key] === obj2[key])
         ? `    ${key}: ${obj1[key]}\n`
@@ -16,7 +24,9 @@ export default (file1, file2) => {
         ? `  - ${key}: ${obj1[key]}\n`
         : `  + ${key}: ${obj2[key]}\n`;
     }
+
     newAcc = acc + newAcc;
+
     return newAcc;
   };
 
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,5 @@
+import path from 'path';
 import fs from 'fs';
-
-const getObjectFromJsonFile = (jsonFile) => JSON.parse(fs.readFileSync(jsonFile, 'utf-8'));
 
 const getSortedKeysWithoutDuplicates = (obj1, obj2) => {
   const keys = [...Object.keys(obj1), ...Object.keys(obj2)];
@@ -9,8 +8,13 @@ const getSortedKeysWithoutDuplicates = (obj1, obj2) => {
 
 const hasKey = (obj, key) => Object.keys(obj).includes(key);
 
+const getData = (file) => fs.readFileSync(file, 'utf-8');
+
+const getFilenameExtension = (file) => path.extname(file);
+
 export {
-  getObjectFromJsonFile,
   getSortedKeysWithoutDuplicates,
   hasKey,
+  getData,
+  getFilenameExtension,
 };
