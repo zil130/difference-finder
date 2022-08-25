8adfe70009fb4e6507a9c417d493fabdf26e65ad
zil130 <index.che@gmail.com>
2022-08-24 21:16:14 -0300

    feat: stylish is defaulted as the formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((file1, file2) => console.log(genDiff(file1, file2)))
+  .action((file1, file2) => console.log(genDiff(file1, file2, program.opts().format)))
   .parse(process.argv);
