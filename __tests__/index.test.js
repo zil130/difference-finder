6e1717836b3d024d1531eece80ea82b4c13e6d67
zil130 <55101149+zil130@users.noreply.github.com>
2022-08-26 01:00:34 -0300

    test: plain-comprassion

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,18 +8,34 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('Comparison of JSON-files with a nested structure', () => {
+test('Stylish-comparison of JSON-files with a nested structure', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
   const diff = genDiff(file1, file2);
-  const correctResult = readFile('expected_file.txt').trim();
+  const correctResult = readFile('expected_stylish.txt').trim();
   expect(diff).toBe(correctResult);
 });
 
-test('Comparison of YAML-files with a nested structure', () => {
+test('Stylish-comparison of YAML-files with a nested structure', () => {
   const file1 = getFixturePath('file1.yaml');
   const file2 = getFixturePath('file2.yml');
   const diff = genDiff(file1, file2);
-  const correctResult = readFile('expected_file.txt').trim();
+  const correctResult = readFile('expected_stylish.txt').trim();
+  expect(diff).toBe(correctResult);
+});
+
+test('Plain-comparison of JSON-files with a nested structure', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  const diff = genDiff(file1, file2);
+  const correctResult = readFile('expected_plain.txt').trim();
+  expect(diff).toBe(correctResult);
+});
+
+test('Plain-comparison of YAML-files with a nested structure', () => {
+  const file1 = getFixturePath('file1.yaml');
+  const file2 = getFixturePath('file2.yml');
+  const diff = genDiff(file1, file2);
+  const correctResult = readFile('expected_plain.txt').trim();
   expect(diff).toBe(correctResult);
 });
