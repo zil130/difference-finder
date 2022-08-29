121b71984c5d9553211ec0d0a47c131cf1f91d49
zil130 <index.che@gmail.com>
2022-08-29 01:34:16 -0300

    feat: add json-formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,1 +1,17 @@
-export default (format) => (format === 'plain' ? 'plain' : 'stylish');
+import stylish from './stylish.js';
+import plain from './plain.js';
+import jsonStringify from './jsonStringify.js';
+
+export default (format) => {
+  let func;
+
+  if (format === 'stylish') {
+    func = stylish;
+  } else if (format === 'plain') {
+    func = plain;
+  } else if (format === 'json') {
+    func = jsonStringify;
+  }
+
+  return func;
+};
