056b78057af27494233de9d062135ae95f188a4d
zil130 <index.che@gmail.com>
2022-09-05 02:05:02 -0300

    test: add new and refactor old

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,50 +2,80 @@ import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
+import getObjectFromData from '../src/parsers.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const getResultOfTest = (file1, file2, format, expectedResult) => {
-  const diff = genDiff(getFixturePath(file1), getFixturePath(file2), format);
-  const correctResult = readFile(expectedResult).trim();
 
-  return expect(diff).toBe(correctResult);
+const expectedStylish = readFile('expected_stylish.txt').trim();
+const expectedPlain = readFile('expected_plain.txt').trim();
+const expectedJson = readFile('expected_json.txt').trim();
+
+const parseResultFromJson = getObjectFromData(readFile('file1.json'), '.json');
+const parseResultFromYaml = getObjectFromData(readFile('file1.yaml'), '.yaml');
+const expectedParse = {
+  common: {
+    setting1: 'Value 1',
+    setting2: 200,
+    setting3: true,
+    setting6: { key: 'value', doge: { wow: '' } }
+  },
+  group1: { baz: 'bas', foo: 'bar', nest: { key: 'value' } },
+  group2: { abc: 12345, deep: { id: 45 } }
 };
 
-test('Stylish-comparison of JSON-files with a nested structure', () => {
-  getResultOfTest('file1.json', 'file2.json', 'stylish', 'expected_stylish.txt');
+describe('JSON-files', () => {
+  const diffNoFormatter = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const diffStylish = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
+  const diffPlain = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
+  const diffJson = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
+
+  test('json parse', () => {
+    expect(parseResultFromJson).toEqual(expectedParse);
+  });
+
+  test('no formatter', () => {
+    expect(diffNoFormatter).toBe(expectedStylish);
+  });
+
+  test('stylish formatter', () => {
+    expect(diffStylish).toBe(expectedStylish);
+  });
+
+  test('plain formatter', () => {
+    expect(diffPlain).toBe(expectedPlain);
+  });
+
+  test('json formatter', () => {
+    expect(diffJson).toBe(expectedJson);
+  });
 });
 
-test('Stylish-comparison of YAML-files with a nested structure', () => {
-  getResultOfTest('file1.yaml', 'file2.yml', 'stylish', 'expected_stylish.txt');
-});
+describe('YAML-files', () => {
+  const diffNoFormatter = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'));
+  const diffStylish = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'), 'stylish');
+  const diffPlain = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'), 'plain');
+  const diffJson = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'), 'json');
 
-test('Stylish-comparison of JSON and YAML-files with a nested structure', () => {
-  getResultOfTest('file1.json', 'file2.yml', 'stylish', 'expected_stylish.txt');
-});
+  test('yaml parse', () => {
+    expect(parseResultFromYaml).toEqual(expectedParse);
+  });
 
-test('Plain-comparison of JSON-files with a nested structure', () => {
-  getResultOfTest('file1.json', 'file2.json', 'plain', 'expected_plain.txt');
-});
+  test('no formatter', () => {
+    expect(diffNoFormatter).toBe(expectedStylish);
+  });
 
-test('Plain-comparison of YAML-files with a nested structure', () => {
-  getResultOfTest('file1.yaml', 'file2.yml', 'plain', 'expected_plain.txt');
-});
+  test('stylish formatter', () => {
+    expect(diffStylish).toBe(expectedStylish);
+  });
 
-test('Plain-comparison of JSON and YAML-files with a nested structure', () => {
-  getResultOfTest('file1.json', 'file2.yml', 'plain', 'expected_plain.txt');
-});
+  test('plain formatter', () => {
+    expect(diffPlain).toBe(expectedPlain);
+  });
 
-test('JSON-comparison of JSON-files with a nested structure', () => {
-  getResultOfTest('file1.json', 'file2.json', 'json', 'expected_json.txt');
-});
-
-test('JSON-comparison of YAML-files with a nested structure', () => {
-  getResultOfTest('file1.yaml', 'file2.yml', 'json', 'expected_json.txt');
-});
-
-test('JSON-comparison of JSON and YAML-files with a nested structure', () => {
-  getResultOfTest('file1.json', 'file2.yml', 'json', 'expected_json.txt');
+  test('json formatter', () => {
+    expect(diffJson).toBe(expectedJson);
+  });
 });
