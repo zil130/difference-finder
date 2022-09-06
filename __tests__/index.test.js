b87d00ee9ca0496f69fe50aaff9f89bc4e0cf7ef
zil130 <index.che@gmail.com>
2022-09-05 23:29:59 -0300

    test: refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,9 +12,6 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 const expectedStylish = readFile('expected_stylish.txt').trim();
 const expectedPlain = readFile('expected_plain.txt').trim();
 const expectedJson = readFile('expected_json.txt').trim();
-
-const parseResultFromJson = getObjectFromData(readFile('file1.json'), '.json');
-const parseResultFromYaml = getObjectFromData(readFile('file1.yaml'), '.yaml');
 const expectedParse = {
   common: {
     setting1: 'Value 1',
@@ -27,55 +24,53 @@ const expectedParse = {
 };
 
 describe('JSON-files', () => {
+  const parseResult = getObjectFromData(readFile('file1.json'), '.json');
   const diffNoFormatter = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
   const diffStylish = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
   const diffPlain = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
   const diffJson = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
 
   test('json parse', () => {
-    expect(parseResultFromJson).toEqual(expectedParse);
+    expect(parseResult).toEqual(expectedParse);
   });
 
-  test('no formatter', () => {
-    expect(diffNoFormatter).toBe(expectedStylish);
-  });
-
-  test('stylish formatter', () => {
-    expect(diffStylish).toBe(expectedStylish);
-  });
-
-  test('plain formatter', () => {
-    expect(diffPlain).toBe(expectedPlain);
-  });
-
-  test('json formatter', () => {
-    expect(diffJson).toBe(expectedJson);
+  describe.each([
+    [diffNoFormatter, 'no formatter', expectedStylish],
+    [diffStylish, 'stylish formatter', expectedStylish],
+    [diffPlain, 'plain formatter', expectedPlain],
+    [diffJson, 'json formatter', expectedJson],
+  ])('', (diff, formatter, expected) => {
+    test(`${formatter}`, () => {
+      expect(diff).toBe(expected);
+      expect(diff).toBe(expected);
+      expect(diff).toBe(expected);
+      expect(diff).toBe(expected);
+    });
   });
 });
 
 describe('YAML-files', () => {
+  const parseResult = getObjectFromData(readFile('file1.yaml'), '.yaml');
   const diffNoFormatter = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'));
   const diffStylish = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'), 'stylish');
   const diffPlain = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'), 'plain');
   const diffJson = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'), 'json');
 
   test('yaml parse', () => {
-    expect(parseResultFromYaml).toEqual(expectedParse);
+    expect(parseResult).toEqual(expectedParse);
   });
 
-  test('no formatter', () => {
-    expect(diffNoFormatter).toBe(expectedStylish);
-  });
-
-  test('stylish formatter', () => {
-    expect(diffStylish).toBe(expectedStylish);
-  });
-
-  test('plain formatter', () => {
-    expect(diffPlain).toBe(expectedPlain);
-  });
-
-  test('json formatter', () => {
-    expect(diffJson).toBe(expectedJson);
+  describe.each([
+    [diffNoFormatter, 'no formatter', expectedStylish],
+    [diffStylish, 'stylish formatter', expectedStylish],
+    [diffPlain, 'plain formatter', expectedPlain],
+    [diffJson, 'json formatter', expectedJson],
+  ])('', (diff, formatter, expected) => {
+    test(`${formatter}`, () => {
+      expect(diff).toBe(expected);
+      expect(diff).toBe(expected);
+      expect(diff).toBe(expected);
+      expect(diff).toBe(expected);
+    });
   });
 });
