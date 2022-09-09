4c7549bb446751e3ea12b4e7a5a140b62020948b
zil130 <index.che@gmail.com>
2022-09-09 19:09:44 -0300

    refactor: removed unnecessary entities

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import buildDiffTree from './buildDiffTree.js';
 import parse from './parser.js';
 import format from './formatters/index.js';
 
-const readFile = (file) => fs.readFileSync(file, 'utf-8');
-
 const extractFormat = (file) => path.extname(file).slice(1);
 
-const getData = (filepath) => {
-  const content = readFile(filepath);
-  const fileFormat = extractFormat(filepath);
-
-  return parse(content, fileFormat);
-};
+const getData = (filepath) => parse(fs.readFileSync(filepath, 'utf-8'), extractFormat(filepath));
 
 export default (filepath1, filepath2, outputFormat = 'stylish') => {
   const data1 = getData(filepath1);
