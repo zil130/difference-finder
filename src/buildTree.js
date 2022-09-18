9a7e9195fc6b35cd1a58f1a9da9fb0492b93c553
zil130 <index.che@gmail.com>
2022-09-18 13:40:26 -0300

    refactor: do not use a data type in the name and parameters

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,27 +1,27 @@
 import _ from 'lodash';
 
-const hasKey = (obj, key) => Object.keys(obj).includes(key);
+const hasKey = (data, key) => Object.keys(data).includes(key);
 
 const isObjects = (value1, value2) => _.isPlainObject(value1) && _.isPlainObject(value2);
 
-const buildTree = (obj1, obj2) => {
-  const uniqueKeys = _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]);
+const buildTree = (data1, data2) => {
+  const uniqueKeys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]);
   const sortedKeys = _.sortBy(uniqueKeys);
 
   return sortedKeys.map((key) => {
-    if (isObjects(obj1[key], obj2[key])) {
-      return [key, { children: buildTree(obj1[key], obj2[key]) }];
+    if (isObjects(data1[key], data2[key])) {
+      return [key, { children: buildTree(data1[key], data2[key]) }];
     }
-    if (hasKey(obj1, key) && hasKey(obj2, key) && obj1[key] === obj2[key]) {
-      return [key, { status: 'unchanged', value: obj2[key] }];
+    if (hasKey(data1, key) && hasKey(data2, key) && data1[key] === data2[key]) {
+      return [key, { status: 'unchanged', value: data2[key] }];
     }
-    if (hasKey(obj1, key) && hasKey(obj2, key) && obj1[key] !== obj2[key]) {
-      return [key, { status: 'changed', value: obj2[key], oldValue: obj1[key] }];
+    if (hasKey(data1, key) && hasKey(data2, key) && data1[key] !== data2[key]) {
+      return [key, { status: 'changed', value: data2[key], oldValue: data1[key] }];
     }
-    if (hasKey(obj1, key) && !hasKey(obj2, key)) {
-      return [key, { status: 'removed', oldValue: obj1[key] }];
+    if (hasKey(data1, key) && !hasKey(data2, key)) {
+      return [key, { status: 'removed', oldValue: data1[key] }];
     }
-    return [key, { status: 'added', value: obj2[key] }];
+    return [key, { status: 'added', value: data2[key] }];
   });
 };
 
