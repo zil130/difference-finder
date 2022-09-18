82c9a7c7058936df9fcb4c014017df7155e0b368
zil130 <index.che@gmail.com>
2022-09-18 15:40:28 -0300

    refactor: removed an unnecessary function isObjects

--- src/buildTree.js
+++ src/buildTree.js
@@ -2,14 +2,12 @@ import _ from 'lodash';
 
 const hasKey = (data, key) => Object.keys(data).includes(key);
 
-const isObjects = (value1, value2) => _.isPlainObject(value1) && _.isPlainObject(value2);
-
 const buildTree = (data1, data2) => {
   const uniqueKeys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]);
   const sortedKeys = _.sortBy(uniqueKeys);
 
   return sortedKeys.map((key) => {
-    if (isObjects(data1[key], data2[key])) {
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return [key, { children: buildTree(data1[key], data2[key]) }];
     }
     if (hasKey(data1, key) && hasKey(data2, key) && data1[key] === data2[key]) {
