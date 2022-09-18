8272526035125c10e232f523f4ae5e2bc65155a6
zil130 <index.che@gmail.com>
2022-09-18 12:59:48 -0300

    refactor: made the parser easier

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,9 @@
 import yaml from 'js-yaml';
 
 const parsers = {
-  json: (data) => JSON.parse(data),
-  yaml: (data) => yaml.load(data, 'utf8'),
-  yml: (data) => yaml.load(data, 'utf8'),
+  json: JSON.parse,
+  yaml: yaml.load,
+  yml: yaml.load,
 };
 
 export default (data, format) => parsers[format](data);
