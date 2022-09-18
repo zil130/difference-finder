8e7a45948803ecd37eb34d52d5552e7a8aef161d
zil130 <55101149+zil130@users.noreply.github.com>
2022-09-18 16:45:42 -0300

    refactor: the custom function has been replaced with a lodash

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,7 +1,5 @@
 import _ from 'lodash';
 
-const hasKey = (data, key) => Object.keys(data).includes(key);
-
 const buildTree = (data1, data2) => {
   const uniqueKeys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]);
   const sortedKeys = _.sortBy(uniqueKeys);
@@ -10,13 +8,13 @@ const buildTree = (data1, data2) => {
     if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return [key, { children: buildTree(data1[key], data2[key]) }];
     }
-    if (hasKey(data1, key) && hasKey(data2, key) && data1[key] === data2[key]) {
+    if (_.has(data1, key) && _.has(data2, key) && data1[key] === data2[key]) {
       return [key, { status: 'unchanged', value: data2[key] }];
     }
-    if (hasKey(data1, key) && hasKey(data2, key) && data1[key] !== data2[key]) {
+    if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
       return [key, { status: 'changed', value: data2[key], oldValue: data1[key] }];
     }
-    if (hasKey(data1, key) && !hasKey(data2, key)) {
+    if (_.has(data1, key) && !_.has(data2, key)) {
       return [key, { status: 'removed', oldValue: data1[key] }];
     }
     return [key, { status: 'added', value: data2[key] }];
