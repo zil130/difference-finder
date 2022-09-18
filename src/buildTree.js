d038257d22571931afb68e205d28f94a4eb82d82
zil130 <index.che@gmail.com>
2022-09-18 13:14:15 -0300

    refactor: removed unnecessary function

--- src/buildTree.js
+++ src/buildTree.js
@@ -4,16 +4,11 @@ const hasKey = (obj, key) => Object.keys(obj).includes(key);
 
 const isObjects = (value1, value2) => _.isPlainObject(value1) && _.isPlainObject(value2);
 
-const getSortedKeysWithoutDuplicates = (obj1, obj2) => {
-  const keys = [...Object.keys(obj1), ...Object.keys(obj2)];
-
-  return _.sortBy(_.uniq(keys));
-};
-
 const buildTree = (obj1, obj2) => {
-  const keys = getSortedKeysWithoutDuplicates(obj1, obj2);
+  const uniqueKeys = _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]);
+  const sortedKeys = _.sortBy(uniqueKeys);
 
-  return keys.map((key) => {
+  return sortedKeys.map((key) => {
     if (isObjects(obj1[key], obj2[key])) {
       return [key, { children: buildTree(obj1[key], obj2[key]) }];
     }
