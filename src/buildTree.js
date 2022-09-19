07c342b6e6784a39fd71000e0e3b6351016dc7f1
zil130 <index.che@gmail.com>
2022-09-18 22:37:56 -0300

    refactor: changed the default return

--- src/buildTree.js
+++ src/buildTree.js
@@ -8,16 +8,16 @@ const buildTree = (data1, data2) => {
     if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return [key, { children: buildTree(data1[key], data2[key]) }];
     }
-    if (_.has(data1, key) && _.has(data2, key) && data1[key] === data2[key]) {
-      return [key, { type: 'unchanged', value: data1[key] }];
-    }
     if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
       return [key, { type: 'changed', value1: data1[key], value2: data2[key] }];
+    }
+    if (!_.has(data1, key)) {
+      return [key, { type: 'added', value: data2[key] }];
     }
     if (!_.has(data2, key)) {
       return [key, { type: 'removed', value: data1[key] }];
     }
-    return [key, { type: 'added', value: data2[key] }];
+    return [key, { type: 'unchanged', value: data1[key] }];
   });
 };
 
