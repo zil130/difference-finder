8fef02fe4270e92c77c0c7ab64f31b22f4e8d58f
zil130 <55101149+zil130@users.noreply.github.com>
2023-06-01 05:56:48 -0300

    refactor: stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,60 +1,59 @@
-const buildStylishTree = (tree) => tree
-  .reduce((acc, prop) => {
+import _ from 'lodash';
+
+const formatObject = (obj, indentLevel = 0) => {
+  const indent = '    '.repeat(indentLevel);
+
+  const keys = Object.keys(obj);
+  const lastIndex = keys.length - 1;
+
+  const lines = keys.map((key, index) => {
+    const value = obj[key];
+    const formattedValue = _.isObject(value) ? formatObject(value, indentLevel + 1) : value;
+    return `    ${indent}${key}: ${formattedValue}${index !== lastIndex ? '\n' : ''}`;
+  });
+
+  return [
+    '{\n',
+    ...lines,
+    `\n${indent}}`,
+  ].join('');
+};
+
+const getValue = (value, indentLevel) => {
+  if (_.isObject(value)) {
+    return formatObject(value, indentLevel);
+  }
+  return value;
+};
+
+const render = (tree, depth) => tree
+  .map((prop) => {
     const [key, keyDescription] = prop;
     const {
       children, type, value, value1, value2,
     } = keyDescription;
+    const spacesCount = 4;
+    const indentSize = depth * spacesCount;
+    const bracketIndent = ' '.repeat((indentSize - spacesCount) < 0 ? 0 : indentSize - spacesCount);
 
     switch (type) {
       case 'unchanged':
-        return { ...acc, [`${key}`]: value };
+        return `${bracketIndent}    ${key}: ${value}`;
       case 'changed':
-        return { ...acc, [`- ${key}`]: value1, [`+ ${key}`]: value2 };
+        return `${bracketIndent}  - ${key}: ${getValue(value1, depth)}\n${bracketIndent}  + ${key}: ${getValue(value2, depth)}`;
       case 'removed':
-        return { ...acc, [`- ${key}`]: value };
+        return `${bracketIndent}  - ${key}: ${getValue(value, depth)}`;
       case 'added':
-        return { ...acc, [`+ ${key}`]: value };
+        return `${bracketIndent}  + ${key}: ${getValue(value, depth)}`;
       case undefined:
-        return { ...acc, [`${key}`]: buildStylishTree(children) };
+        return `${bracketIndent}    ${key}: {\n${render(children, depth + 1).join('\n')}\n${bracketIndent}    }`;
       default:
         throw new Error(`Unknown type: '${type}'`);
     }
-  }, {});
+  });
 
-const render = (data) => {
-  const iter = (currentValue, depth) => {
-    if (typeof currentValue !== 'object' || currentValue === null) {
-      return String(currentValue);
-    }
-
-    if (Array.isArray(currentValue)) {
-      return `[${currentValue}]`;
-    }
-
-    const spacesCount = 4;
-    const indentSize = depth * spacesCount;
-    const bracketIndent = ' '.repeat(indentSize - spacesCount);
-    const lines = Object
-      .entries(currentValue)
-      .map(([key, val]) => {
-        const currentIndent = (key.substring(0, 2) === '+ ' || key.substring(0, 2) === '- ')
-          ? ' '.repeat(indentSize - 2)
-          : ' '.repeat(indentSize);
-
-        return `${currentIndent}${key}: ${iter(val, depth + 1)}`;
-      });
-
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-
-  return iter(data, 1);
-};
-
-export default (tree) => {
-  const stylishTree = buildStylishTree(tree);
-  return render(stylishTree);
-};
+export default (tree) => [
+  '{',
+  ...render(tree, 1),
+  '}',
+].join('\n');
